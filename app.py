6ad8d572d4b7649fea1fe1be04fcbcbaff320db3
AnushkaSoni-tech <anushka.s812006@gmail.com>
2026-02-20 00:23:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="Health Prediction System", page_icon="🩺", layout="centered")
 
 st.title("Health Prediction System")
-age = st.number_input("Age (years)", min_value=1, max_value=0, value=20)
-weight = st.number_input("Weight (kg)", min_value=1.0, max_value=300.0, value=0, step=0.1)
-height = st.number_input("Height (m)", min_value=0.5, max_value=2.5, value=0, step=0.01)
+age = st.number_input("Age (years)", min_value=1, max_value=200,step=1)
+weight = st.number_input("Weight (kg)", min_value=1.0, max_value=300.0,  step=0.1)
+height = st.number_input("Height (cm)", min_value=0.5, max_value=2.5, step=0.01)
 gender=st.selectbox("Gender",["Male","Female","Other"])
+
